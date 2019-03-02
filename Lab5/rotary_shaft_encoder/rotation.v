4afc7f27ab919e5d664efae2568fbd65b1af3220
Chaman Agrawal <chaman.ag@gmail.com>
2019-03-02 15:25:17 +0530

    Update rotation.v
    Change blocking to non-blocking assignment

--- Lab5/rotary_shaft_encoder/rotation.v
+++ Lab5/rotary_shaft_encoder/rotation.v
@@ -28,16 +28,16 @@ module rotation( clk, ROT_A, ROT_B, rotation_event, rotation_direction
 	 
 	 always@(posedge clk) begin
 		if(ROT_A == 1 && ROT_B == 1) begin
-				rotation_event = 1;
+				rotation_event <= 1;
 		end
 		else if(ROT_A == 0 && ROT_B == 0) begin
-				rotation_event = 0;
+				rotation_event <= 0;
 		end
 		else if(ROT_A == 0 && ROT_B == 1) begin
-				rotation_direction = 1;
+				rotation_direction <= 1;
 		end
 		else begin
-				rotation_direction = 0;
+				rotation_direction <= 0;
 		end
 	 end
 endmodule
