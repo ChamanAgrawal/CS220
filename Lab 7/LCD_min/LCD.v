d1281c82d1837102dadf4940dc3709a3d70998ac
RishabhB99 <46969858+RishabhB99@users.noreply.github.com>
2019-03-11 00:26:58 +0530

    Add files via upload

---
+++ Lab 7/LCD_min/LCD.v
@@ -0,0 +1,337 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:22:00 03/10/2019 
+// Design Name: 
+// Module Name:    LCD 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module LCD(DB,clk,lcd_rs,lcd_rw,lcd_e,data
+    );
+
+	input min;
+	input[7:0] DB;
+	input clk;
+	output lcd_rs,lcd_rw,lcd_e,data;
+	reg lcd_rs, lcd_rw, lcd_e;
+	reg[3:0] data;
+	reg[20:0] counter = 21'b0;
+	reg[3:0] state = 4'b0; 
+	always@(posedge clk) begin
+		if(counter == 2000000) begin
+			case(state)
+			0: begin
+				case(step)
+				0: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				1: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b0011;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				2: begin
+					lcd_e <= 1;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				3: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				4: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b0011;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				5: begin
+					lcd_e <= 1;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				6: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				7: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b0010;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				8: begin
+					lcd_e <= 0;
+					step <= 4'b0;
+					counter <= 21'b0;
+					step <= 4'b0;
+				end	
+				endcase
+				state <= state+1;
+			end	
+			1: begin
+				case(step)
+				0: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				1: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b0010;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				2: begin
+					lcd_e <= 1;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				3: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				4: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b1000;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				5: begin
+					lcd_e <= 0;
+					step <= 4'b0;
+					counter <= 21'b0;
+					step <= 4'b0;
+				end	
+				endcase
+				state <= state+1;
+			end
+			2: begin
+				case(step)
+				0: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				1: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				2: begin
+					lcd_e <= 1;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				3: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				4: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b0110;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				5: begin
+					lcd_e <= 0;
+					step <= 4'b0;
+					counter <= 21'b0;
+					step <= 4'b0;
+				end	
+				endcase
+				state <= state+1;
+			end
+			3: begin
+				case(step)
+				0: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				1: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				2: begin
+					lcd_e <= 1;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				3: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				4: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b1100;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				5: begin
+					lcd_e <= 0;
+					step <= 4'b0;
+					counter <= 21'b0;
+					step <= 4'b0;
+				end	
+				endcase
+				state <= state+1;
+			end
+			4: begin
+				case(step)
+				0: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				1: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				2: begin
+					lcd_e <= 1;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				3: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				4: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b0001;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				5: begin
+					lcd_e <= 0;
+					step <= 4'b0;
+					counter <= 21'b0;
+					step <=4'b0;
+				end	
+				endcase
+				state <= state+1;
+			end
+			5: begin
+				case(step)
+				0: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				1: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b1000;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				2: begin
+					lcd_e <= 1;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				3: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				4: begin
+					lcd_rs <= 0;
+					lcd_w <= 0;
+					data <= 4'b0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				5: begin
+					lcd_e <= 0;
+					step <= 4'b0;
+					counter <= 21'b0;
+					step <=4'b0;
+				end	
+				endcase
+				state <= state+1;
+			end
+			6: begin
+				case(step)
+				0: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				1: begin
+					lcd_rs <= 1;
+					lcd_w <= 0;
+					data <= DB[7:4];
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				2: begin
+					lcd_e <= 1;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				3: begin
+					lcd_e <= 0;
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				4: begin
+					lcd_rs <= 1;
+					lcd_w <= 0;
+					data <= DB[3:0];
+					step <= step+1;
+					counter <= 21'b0;
+				end	
+				5: begin
+					lcd_e <= 0;
+					step <= 4'b0;
+					counter <= 21'b0;
+					step <=4'b0;
+				end	
+				endcase
+			end
+		endcase
+		end
+		else begin
+			counter <= counter+1;
+		end	
+	end	
+endmodule
--- Lab 7/LCD_min/minimum.v
+++ Lab 7/LCD_min/minimum.v
@@ -24,20 +24,24 @@ input [2:0] A, B, C, D;
 input clk;
 output minpos;
 reg[1:0] minpos;
-
+reg[7:0] DB = 8'b0;
 always @(posedge clk) begin
 	if ((A <= B) && (A <= C) && (A <= D)) begin
 		minpos <= 0;
+		DB <= 8'b00110000;
 	end
 	else if ((B <= A) && (B <= C) && (A <= D)) begin
 		minpos <= 1;
+		DB <= 8'b00110001;
 	end
 	else if ((C <= A) && (C <= B) && (A <= D)) begin
 		minpos <= 2;
+		DB <= 8'b00110010;
 	end
 	else if ((D <= A) && (D <= B) && (D <= C)) begin
 		minpos <= 3;
+		DB <= 8'b00110011;
 	end
 end
-
+	LCD uut0(DB,clk,lcd_rs,lcd_rw,lcd_e,data);
 endmodule
